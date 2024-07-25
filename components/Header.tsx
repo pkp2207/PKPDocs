d9148bdd8bd07f4f2f239b9fa1a4a92bd13d21c3
Param Pathak <parampathak47@gmail.com>
2024-07-25 22:20:11 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header = ({ children, className }: HeaderProps) => {
     <div className={cn("header", className)}>
       <Link href='/' className="md:flex-1">
         <Image 
-          src="/assets/icons/logo.svg"
+          src="/assets/icons/logo-icon.svg"
           alt="Logo with name"
           width={120}
           height={32}
